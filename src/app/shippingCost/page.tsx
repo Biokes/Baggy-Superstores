371fdc50ee3adbbb88855516880cd00c9f6268e2
Biokes <106444765+Biokes@users.noreply.github.com>
2024-10-17 00:57:51 +0100

    refactor shopping component

--- src/app/shippingCost/page.tsx
+++ src/app/shippingCost/page.tsx
@@ -1,57 +1,9 @@
-'use client'
-import styles from '@/styles/index.module.css'
-import Navbar from "@/components/homePage/navbar"
-import Footer from '@/components/homePage/footer'
-export default function Shipping(){
+import Shipping from "@/components/shoppingCost/shoppinfgCost";
+
+export default function ShippingCostPage(){
     return (
         <div>
-            <Navbar props={8}/>
-            <div className={'flex flex-col gap-4 my-[20px]'}>
-                <p className={styles.bestSellers}>Delivery Costs</p>
-                <div className={`flex flex-col items-start ${styles.ptags}`}>
-                    <p>
-                        1. Lorem ipsum dolor sit amet consectetur adipiscing elit hendrerit vulputate,
-                        fermentum pellentesque feugiat aenean fringilla torquent malesuada.
-                        Taciti fames vehicula aliquam arcu nascetur habitasse dictum conubia eleifend,
-                        proin netus magna mollis pulvinar nostra habitant suscipit quisque, eu molestie fusce et mi in adipiscing ad.
-                        Commodo efficitur ac quisque in taciti arcu volutpat condimentum a tortor, malesuada facilisi
-                        etiam congue dictum finibus nulla nisi lorem, metus inceptos cras ridiculus curabitur
-                    </p>
-                    <p>
-                        2. Erat eget ante vulputate. Consectetur felis finibus nibh eleifend curae rutrum class ultrices velit,
-                        ante volutpat ex cras semper at pretium conubia sodales,
-                        interdum torquent varius malesuada facilisi dictum nec metus.
-                        Lacinia suspendisse magnis turpis massa etiam iaculis est, 
-                        nam magna ultricies leo himenaeos scelerisque natoque viverra, 
-                        lorem libero commodo tellus aliquet ornare. Class mus sit odio vitae scelerisque augue, 
-                        dignissim ornare et eros a proin, fringilla varius turpis curae tristique. 
-                        Commodo cras phasellus efficitur facilisi sit elit interdum augue litora fusce, 
-                        felis odio magnis taciti et diam potenti curae penatibus platea,
-                        amet facilisis turpis ipsum ligula class aliquet ex eros.
-                    </p>
-                    <p>
-                        3. Euismod ex tempor viverra lacinia etiam facilisis nulla venenatis donec,
-                        eleifend aliquam himenaeos curabitur quam elit odio fringilla, quis amet adipiscing 
-                        ligula congue montes vulputate luctus. Odio lacus ullamcorper vestibulum tincidunt est 
-                        varius libero mi, imperdiet nisi turpis maecenas venenatis eleifend tellus, tempus inceptos 
-                        sit ligula at arcu dictumst. Primis eleifend neque mauris tristique venenatis ante odio, justo 
-                        felis facilisi habitasse sapien id vehicula, pretium scelerisque nascetur enim per tincidunt.
-                    </p>
-                    <p>
-                        4. Quam class risus adipiscing dui felis a aliquet, cras aptent pellentesque lorem vivamus tempus,
-                        velit inceptos porta sit bibendum dis. Ullamcorper pulvinar blandit arcu augue nulla senectus donec 
-                        sociosqu nullam, id commodo inceptos habitasse lacus suscipit tempor lorem viverra, nisi dui facilisis
-                        porttitor ultrices magnis erat turpis. 
-                    </p>
-                    <p>
-                        5. Quam class risus adipiscing dui felis a aliquet, cras aptent pellentesque lorem vivamus tempus,
-                        velit inceptos porta sit bibendum dis. Ullamcorper pulvinar blandit arcu augue nulla senectus donec 
-                        sociosqu nullam, id commodo inceptos habitasse lacus suscipit tempor lorem viverra, nisi dui facilisis
-                        porttitor ultrices magnis erat turpis.
-                    </p>
-                </div>
-            </div>
-            <Footer/>
+            <Shipping/>
         </div>
     )
-}       
+}
---
+++ src/components/shopping/Shopping.tsx
@@ -0,0 +1,64 @@
+'use client'
+
+import { useDispatch } from "react-redux"
+import Navbar from '@/components/homePage/navbar';
+import styles from '@/styles/index.module.css'
+import small1 from '../../../assets/BAGS015-lu101mjc.webp';
+import small2 from '../../../assets/BAGS012-lu101osh.webp';
+import bag1 from '../../../assets/BAGS11-lu101ode.webp';
+import bag2 from '../../../assets/BAGS022-lu101neq.webp';
+import bag3 from '../../../assets/BAGS020-lu101o3d.webp';
+import bag4 from '../../../assets/BAGS025-lu101ol1.webp';
+import bag5 from '../../../assets/BAGS014-lu101n4m.webp';
+import bag6 from '../../../assets/BAGS008-lu101nvj.webp';
+import bag7 from '../../../assets/BAGS021-lu101m39.webp'
+import bag8 from '../../../assets/BAGS012-lu101map.webp';
+import bag9 from '../../../assets/BAGS012-lu101osh (1).webp';
+import bag10 from '../../../assets/BAGS015-lu101mjc (1).webp';
+import { BagDetails } from "@/interfaces/interfaces";
+import { setBag } from "@/redux/bagSlice";
+import Image from 'next/image';
+import Footer from '@/components/homePage/footer';
+import Link from 'next/link'
+
+export default function Shopping(){
+    const dispatch = useDispatch()
+    const handleClick = (bag: BagDetails) => {
+        dispatch(setBag(bag));
+    };
+    const bagsPack=[
+        {image:small1,store:'Emily Bags',price:'$30'},
+        {image:small2,store:'Fav Bags',price:'$30'},
+        {image:bag1,store:'Catherine Bags',price:'$30'},
+        {image:bag2,store:'Mary Bags',price:'$30'},
+        {image:bag3,store:'Rite Bags',price:'$30'},
+        {image:bag4,store:'Jenny Bags',price:'$30'},
+        {image:bag5,store:'Lily Bags',price:'$30'},
+        {image:bag6,store:'Rose Bags',price:'$30'},
+        {image:bag7, store:'covered bags', price: '$80'},
+        {image:bag8,store:'maven bags', price:'$40'},
+        {image:bag9, store:'Baggy bags', price:'$60'},
+        {image:bag10, store:'Suzzy bags', price:'$25'}
+    ]
+    return (
+        <div>
+            <Navbar props={2}/>
+            <p className={`mt-[20px] ${styles.bestSellers}`}>Shopping Bags</p>
+            <div className={`${styles.backPack}`}>
+                {
+                    bagsPack.map((bag, index) => (
+                        <Link key={index} className={`sm:order-${index + 1}`}
+                              onClick={() => handleClick(bag)} href={'/backpack'}>
+                            <div className={`__zoom-enter ${styles.bagImages}`}>
+                                <Image src={bag.image} className={`${styles.images} __zoom-enter`} alt='' />
+                            </div>
+                            <p className={styles.storename}>{bag.store}</p>
+                            <p className={styles.storeText}>{bag.price}</p>
+                        </Link>
+                    ))
+                }
+            </div>
+            <Footer/>
+        </div>
+    )
+}
